9849c5c7c7c9e97588c1621c79d9e7c3407c8bfc
Soroush Ahrari <ahrari.soroush76@gmail.com>
2021-02-07 10:07:20 +0330

    add bfs algorithm

--- bfs.ts
+++ bfs.ts
@@ -1,7 +1,36 @@
-//Todo: Implemen
+import { Problem } from './problem';
+export class BFS {
+    public problem: Problem;
+    constructor(problem: Problem) {
+        this.problem = problem;
+    }
 
-//Todo: Implement BFS Algorithm
 
-//Todo: Implement DFS Algorithm
+    algorithm(initialState: number[][]) {
 
-//Todo: Implement A* Algorithm
+        let states = [initialState];
+
+        while (states.length > 0) {
+            const currentState: number[][] = states.pop()!;
+
+
+            if (this.problem.isGoal(currentState)) {
+                return currentState;
+            }
+
+            const nextActions = this.problem.actions(currentState);
+
+            nextActions.map(action => {
+                const newState = this.problem.place(currentState, action[0], action[1]);
+                states.push(newState);
+            })
+        }
+        return initialState;
+    }
+
+    solve(problem: Problem) {
+        const initialState = problem.initital;
+        return this.algorithm(initialState);
+    }
+
+}
