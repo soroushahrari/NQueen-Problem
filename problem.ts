f776cdf6f4bb3ff41664aff2f448e2764b3b9b16
Soroush Ahrari <ahrari.soroush76@gmail.com>
2021-02-09 16:33:57 +0330

    add some methods for hillclimbing algorithm

--- problem.ts
+++ problem.ts
@@ -1,11 +1,62 @@
 export class Problem {
     public initital =
         [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0], 
+            [0, 0, 0, 0], 
         ];
+
+    initRandom(initialState: number[][]): number[][] {
+        initialState.map(row => {
+            row[Math.round(Math.random() * 3)] = 1;
+        })
+
+        return initialState;
+    }
+
+    heuristic(state: number[][]) {
+        let conflicts = 0;
+        for (let i = 0; i < state.length; i++) {
+            for (let j = 0; j < state.length; j++) {
+                if (state[i][j] == 1) {
+                    for (let k = i + 1; k < state.length; k++) {
+                        for (let l = 0; l < state.length; l++) {
+                            if (state[k][l] == 1) {
+                                if (this.hasConflict(i, j, k, l)) {
+                                    conflicts += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return conflicts;
+    }
+
+    neighbours(state: number[][]) {
+        let neighbours = [];
+        for (let i = 0; i < state.length; i++) {
+            let queen: number = -1;
+            for (let j = 0; j < state.length; j++) {
+                if (state[i][j] == 1) {
+                    queen = j;
+                }
+            }
+            if (queen > -1) {
+                for (let k = 0; k < state.length; k++) {
+                    if (k != queen) {
+                        const newState: number[][] = JSON.parse(JSON.stringify(state));
+                        newState[i][k] = 1;
+                        newState[i][queen] = 0;
+                        neighbours.push(newState);
+                    }
+                }
+            }
+        }
+        return neighbours;
+    }
 
     actions(state: number[][]) {
         const actions = [];
@@ -13,12 +64,15 @@ export class Problem {
             const rowSum = state[i].reduce((a, b) => a + b, 0);
             if (rowSum == 1) {
                 continue;
-            }
-            for (let j = 0; j < state.length; j++) {
-                if (state[i][j] != 1) {
-                    if (this.isConflicted(state, i, j) == false) actions.push([i, j]);
+            } else {
+                for (let j = 0; j < state.length; j++) {
+                    if (state[i][j] != 1) {
+                        if (this.isConflicted(state, i, j) == false) actions.push([i, j]);
+                    }
                 }
+                break;
             }
+            
         }
         return actions;
     }
